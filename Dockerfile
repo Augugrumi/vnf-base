cb2242a7128974f7e8f71b52c1601b076e7a6c40
Davide Polonio <poloniodavide@gmail.com>
2018-07-03 10:52:32 +0000

    Add new code

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,25 @@ FROM alpine:3.7
 LABEL Base image for VNF launched from Openbaton
 
 LABEL maintainer="poloniodavide@gmail.com"
+LABEL license="MIT"
 
+# TODO: create env variable with predefined values.
 ENV  LANG=en_US.UTF-8 \
      LANGUAGE=en_US.UTF-8 \
      LC_COLLATE=C \
-     LC_CTYPE=en_US.UTF-8
+     LC_CTYPE=en_US.UTF-8 \
+     ZABBIX_SERVER_PORT=10050 \
 
-# TODO: create supervisord config to launch ems anf zabbit client
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY zabbixconfig.sh /usr/bin/zabbixconfig
 
-RUN apk add --no-cache --virtual py2-pip git && \
+RUN apk add --no-cache --virtual py2-pip git zabbix-agent net-snmp && \
     pip install --upgrade pip && \
     pip install pika && \
     pip install gitpython && \
+    pip install supervisor && \
     cp /usr/share/zoneinfo/$TIMEZONE /etc/localtime && \
     mkdir /opt/openbaton && \
     pip install openbaton-ems && \
-    # TODO: install zabbix client
+
+ENTRYPOINT ["supervisord", "--nodaemon", "--configuration", "/etc/supervisor/conf.d/supervisord.conf"]
---
+++ zabbixconfig.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+readonly ZABBIX_CONFIG_FILE_PATH="/etc/zabbix/zabbix_agentd.conf"
+
+function check () {
+    if [ "$1" -ne 0 ]
+    then
+        msg err "$2"
+        exit 1
+    fi
+}
+
+function msg () {
+    # 3 type of messages:
+    # - info
+    # - warn
+    # - err
+    local color=""
+    local readonly default="\033[m" #reset
+    if [ "$1" = "info" ]
+    then
+        color="\033[0;32m" #green
+    elif [ "$1" = "warn" ]
+    then
+        color="\033[1;33m" #yellow
+    elif [ "$1" = "err" ]
+    then
+        color="\033[0;31m" #red
+    fi
+
+    echo -e "$color==> $2$default"
+}
+
+msg info "Configurating zabbix agent"
+
+if [ -a $ZABBIX_CONFIG_FILE_PATH ]
+then
+    msg info "File already existing, deleting it..."
+    rm $ZABBI_CONFIG_FILE_PATH
+else
+    msg info "Creating a new config file..."
+fi
+
+cat <<EOF | tee $ZABBIX_CONFIG_FILE_PATH
+Server=$ZABBIX_SERVER_IP
+Hostname=$(awk 'END{print $1}' /etc/hosts)
+ListenPort=$ZABBIX_SERVER_PORT
+EOF
+
+check $? "Impossible to create configuration file!"
+msg info "Configuration file created successfully"
+exit 0
